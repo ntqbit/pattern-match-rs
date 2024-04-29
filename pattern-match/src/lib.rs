9d56f1a046c77f4a16830a689c29e3c1e8987498
ntqbit <ntqbit@proton.me>
2024-04-29 22:55:13 +0300

    Update

--- pattern-match/src/lib.rs
+++ pattern-match/src/lib.rs
@@ -49,60 +49,48 @@ fn pattern_to_bytes(pat: Pattern<'_>) -> Vec<u8> {
     pat.into_iter().map(|x| x.byte).collect()
 }
 
-trait MultipleSearch {
-    // Search for multiple byte-patterns.
-    // Returns the list of matches M, where i is the index of the pattern in `patterns`,
-    // j is the index of the match, M[i][j] is the offset in the haystack of the start of the match.
-    fn search_multiple(&self, haystack: &[u8], patterns: &[&[u8]]) -> Vec<Vec<usize>>;
-}
+pub fn find_overlapping_all(pattern: Pattern<'_>, haystack: &[u8]) -> Vec<usize> {
+    // For each pattern, find the best representing atom to search for.
+    let (start, end, quality) = find_best_atom(pattern).unwrap();
+    // TODO: fallback
+    assert!(quality >= ATOM_QUALITY_THRESHOLD);
+    let atom_bytes = pattern_to_bytes(&pattern[start..end]);
 
-trait SingleSearch {
-    #[allow(dead_code)]
-    fn search_single(&self, haystack: &[u8], pattern: Pattern<'_>) -> Vec<usize>;
-}
+    // Find atom matches.
+    let ac = aho_corasick::AhoCorasick::new(&[atom_bytes.as_ref() as &[u8]]).unwrap();
+    let mut matches = Vec::new();
 
-struct AhoCorasickMultipleSearch;
-
-impl MultipleSearch for AhoCorasickMultipleSearch {
-    fn search_multiple(&self, haystack: &[u8], patterns: &[&[u8]]) -> Vec<Vec<usize>> {
-        let ac = aho_corasick::AhoCorasick::new(patterns).unwrap();
-        let mut matches = vec![Vec::new(); patterns.len()];
-
-        for m in ac.find_overlapping_iter(haystack) {
-            let pattern_id = m.pattern().as_usize();
-            matches[pattern_id].push(m.start());
+    for m in ac.find_overlapping_iter(haystack) {
+        let offset = m.start() - start;
+        if match_haystack(pattern, &haystack[offset..offset + pattern.len()]) {
+            matches.push(offset);
         }
-
-        matches
     }
+
+    matches
 }
 
-struct KmpMultipleSearch;
+pub fn find_one(pattern: Pattern<'_>, haystack: &[u8]) -> Option<usize> {
+    // For each pattern, find the best representing atom to search for.
+    let (start, end, quality) = find_best_atom(pattern).unwrap();
+    // TODO: fallback
+    assert!(quality >= ATOM_QUALITY_THRESHOLD);
+    let atom_bytes = pattern_to_bytes(&pattern[start..end]);
 
-impl MultipleSearch for KmpMultipleSearch {
-    fn search_multiple(&self, haystack: &[u8], patterns: &[&[u8]]) -> Vec<Vec<usize>> {
-        patterns
-            .into_iter()
-            .map(|&pat| kmp::kmp_match(pat, haystack))
-            .collect()
+    // Find atom matches.
+    let ac = aho_corasick::AhoCorasick::new(&[atom_bytes.as_ref() as &[u8]]).unwrap();
+
+    for m in ac.find_overlapping_iter(haystack) {
+        let offset = m.start() - start;
+        if match_haystack(pattern, &haystack[offset..offset + pattern.len()]) {
+            return Some(offset);
+        }
     }
+
+    None
 }
 
-struct UnimplementedSingleSearch;
-
-impl SingleSearch for UnimplementedSingleSearch {
-    fn search_single(&self, _haystack: &[u8], _pattern: Pattern<'_>) -> Vec<usize> {
-        unimplemented!()
-    }
-}
-
-// Searches for multiple masked byte patterns.
-fn find_multiple_overlapping_inner(
-    haystack: &[u8],
-    patterns: &[Pattern<'_>],
-    multiple: impl MultipleSearch,
-    _fallback: impl SingleSearch,
-) -> Vec<Vec<usize>> {
+pub fn find_multiple_overlapping(patterns: &[Pattern<'_>], haystack: &[u8]) -> Vec<Vec<usize>> {
     // For each pattern, find the best representing atom to search for.
     let best_atoms: Vec<(usize, Vec<u8>)> = patterns
         .into_iter()
@@ -118,66 +106,18 @@ fn find_multiple_overlapping_inner(
     let best_atoms_ref: Vec<&[u8]> = best_atoms.iter().map(|(_, v)| v.as_ref()).collect();
 
     // Find atom matches.
-    let mut matches = multiple.search_multiple(haystack, &best_atoms_ref);
+    let ac = aho_corasick::AhoCorasick::new(&best_atoms_ref).unwrap();
+    let mut matches = vec![Vec::new(); patterns.len()];
 
-    // Perform exactt pattern matches.
-    matches.iter_mut().enumerate().for_each(|(i, m)| {
-        let pat = patterns[i];
-        let atom_start = best_atoms[i].0;
-
-        // Filter out atom matches that do not match the entire pattern.
-        m.retain_mut(|offset| {
-            *offset -= atom_start;
-            let offset = *offset;
-            match_haystack(pat, &haystack[offset..offset + pat.len()])
-        });
-    });
+    for m in ac.find_overlapping_iter(haystack) {
+        let pattern_id = m.pattern().as_usize();
+        let pat = patterns[pattern_id];
+        let atom_start = best_atoms[pattern_id].0;
+        let offset = m.start() - atom_start;
+        if match_haystack(pat, &haystack[offset..offset + pat.len()]) {
+            matches[pattern_id].push(offset);
+        }
+    }
 
     matches
-}
-
-fn find_single_overlapping_inner(
-    haystack: &[u8],
-    pattern: Pattern<'_>,
-    multiple: impl MultipleSearch,
-    _fallback: impl SingleSearch,
-) -> Vec<usize> {
-    let (start, end, quality) = find_best_atom(pattern).unwrap();
-    // TODO: fallback
-    assert!(quality >= ATOM_QUALITY_THRESHOLD);
-    let atom_bytes = pattern_to_bytes(&pattern[start..end]);
-
-    // Find atom matches.
-    let mut m = multiple
-        .search_multiple(haystack, &[atom_bytes.as_ref()])
-        .pop()
-        .unwrap();
-
-    // Perform exact pattern matches.
-    // Filter out atom matches that do not match the entire pattern.
-    m.retain_mut(|offset| {
-        *offset -= start;
-        let offset = *offset;
-        match_haystack(pattern, &haystack[offset..offset + pattern.len()])
-    });
-
-    m
-}
-
-pub fn find_overlapping(pattern: Pattern<'_>, haystack: &[u8]) -> Vec<usize> {
-    find_single_overlapping_inner(
-        haystack,
-        pattern,
-        KmpMultipleSearch,
-        UnimplementedSingleSearch,
-    )
-}
-
-pub fn find_multiple_overlapping(patterns: &[Pattern<'_>], haystack: &[u8]) -> Vec<Vec<usize>> {
-    find_multiple_overlapping_inner(
-        haystack,
-        patterns,
-        AhoCorasickMultipleSearch,
-        UnimplementedSingleSearch,
-    )
 }
